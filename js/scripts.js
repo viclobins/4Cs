988fa8f545afb9c624fe4063aaaea0db93a434d4
viclobins <vic@thisisrare.co.uk>
2018-06-28 12:19:02 +0100

    First via upload

---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+	
+	"use strict";
+	
+	var $window = $(window),
+		$animation_elements = $('.icon, .title-card .text, .photo-panel h2'),
+		$photo = $('.photo-panel');
+	
+	$photo.each(function(i, el){
+		var photoFromTop = $(this).offset().top;
+		
+		$window.scroll(function(){
+			var scrolledY = $window.scrollTop();
+			
+			if( scrolledY > photoFromTop ) {
+				$(el).addClass('fixed');
+			} else {
+				$(el).removeClass('fixed');
+			}
+		});
+	});
+
+	function check_if_in_view() {
+	  var window_height = $window.height();
+	  var window_top_position = $window.scrollTop();
+	  var window_bottom_position = (window_top_position + window_height);
+
+	  $.each($animation_elements, function() {
+		var $element = $(this);
+		var element_height = $element.outerHeight();
+		var element_top_position = $element.offset().top;
+		var element_bottom_position = (element_top_position + element_height);
+
+		//check to see if this current container is within viewport
+		if ((element_bottom_position >= window_top_position) &&
+		  (element_top_position <= window_bottom_position)) {
+		  $element.addClass('in-view');
+		} else {
+		  $element.removeClass('in-view');
+		}
+	  });
+	}
+
+	$window.on('scroll resize', check_if_in_view);
+	$window.trigger('scroll');
+	
+});
